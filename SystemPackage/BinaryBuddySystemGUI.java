25c868c610206c41d5c032638a404ae4220d2795
biggst0 <biggst0@students.rowan.edu>
2015-05-04 22:05:53 -0400

    GUI Update

--- SystemPackage/BinaryBuddySystemGUI.java
+++ SystemPackage/BinaryBuddySystemGUI.java
@@ -10,6 +10,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+/**
+ * BinaryBuddySystemGUI
+ * Creates a graphical Representation of the Binary Buddy System
+ * @author Travis
+ */
+
 public class BinaryBuddySystemGUI 
 {
 	private ArrayList<Memory> memArray;
@@ -17,19 +23,22 @@ public class BinaryBuddySystemGUI
 	private JPanel listPane;
 	private JFrame frame;
 	
-	private int heightAdjustment = 28;
-	private final int MEM_BLOCK_SCALE = 10; 
-	private final int MEM_BLOCK_WIDTH = 192; 
+	private int heightAdjustment = 26;
+	private final int MEM_BLOCK_SCALE = 13; 
+	private final int MEM_BLOCK_WIDTH = 256; 
 	private final int MEM_BLOCK_WIDTH_ADJUST = 16; 
+	private final int INITIAL_HEIGHT = 64; 
 	
+	/**
+	 * Creates a new BinaryBuddySystemGUI
+	 * 
+	 */
 	public BinaryBuddySystemGUI()
 	{
 		
-		
 		frame = new JFrame();
 	
-		frame.setPreferredSize( new Dimension( MEM_BLOCK_WIDTH + MEM_BLOCK_WIDTH_ADJUST , 64 * MEM_BLOCK_SCALE + heightAdjustment) );
-		//frame.setSize(MEM_BLOCK_WIDTH, 64 * MEM_BLOCK_SCALE); // for testing
+		frame.setPreferredSize(new Dimension(MEM_BLOCK_WIDTH + MEM_BLOCK_WIDTH_ADJUST , INITIAL_HEIGHT));
 		listPane = new JPanel();
 		listPane.setLayout(new FlowLayout());
 		listPane.setBackground(Color.BLACK);
@@ -37,40 +46,65 @@ public class BinaryBuddySystemGUI
 		frame.setAlwaysOnTop(true);
 		frame.add(listPane);
 		frame.pack();
+		frame.setLocation(64,64);	
+		listPane.add(startPannel());
 		listPane.setVisible(true);
-		frame.setVisible(true);
-		
+		frame.setVisible(true);	
 	}
 	
+	
+	/**
+	 * Redraws the BinaryBuddySystemGUI
+	 * If the memArray contains 1 or less memory objects the size of the GUI is adjusted.
+	 * Cycles through the memArray to build a number of memory panels based on the memArray size.
+	 * @param maxHeight Height relating to the amount of memory objects in the memory Array.
+	 * @param memArray The Array of memory objects
+	 */
 	public void drawGUI(int maxHeight, ArrayList<Memory> memArray)
 	{
 		if(memArray.size() <= 1)
 		{
-			heightAdjustment = 28;
+			heightAdjustment = 26;
 		}
 		else
 		{
-			heightAdjustment = 8;
+			heightAdjustment = 0;
 		}
+		
 		listPane.removeAll();
 		frame.setPreferredSize(new Dimension(MEM_BLOCK_WIDTH + MEM_BLOCK_WIDTH_ADJUST, (maxHeight * MEM_BLOCK_SCALE) + (memArray.size() * MEM_BLOCK_SCALE) + heightAdjustment));
 		listPane.setBackground(Color.BLACK);
 		
 		for(Memory mem: memArray) 
 		{
-			listPane.add(createPnnel(mem));	
+			listPane.add(createPannel(mem));
 		}
 		
 		frame.pack();
 		frame.repaint();
 		frame.revalidate();
-		
 	}
 	
-	public JTextArea createPnnel(Memory mem)
+	/**
+	 * Builds a specific memory JTextArea based on the data provided.
+	 * Green means active.
+	 * Red means not Active.
+	 * Provides memory information.
+	 * @param mem Current memory object that is being represented by this JTextArea.
+	 * @return returns the JTextArea.
+	 */
+	public JTextArea createPannel(Memory mem)
 	{
 		JTextArea thisTextArea = new JTextArea();
-		thisTextArea.setPreferredSize(new Dimension(MEM_BLOCK_WIDTH, mem.getValue() * MEM_BLOCK_SCALE));
+		
+		int power = 2;        
+		while (power < mem.getValue()) 
+		{
+			power = power * 2;
+		}
+		int blockAsPowerTwo = power;
+		
+		thisTextArea.setPreferredSize(new Dimension(MEM_BLOCK_WIDTH, blockAsPowerTwo * MEM_BLOCK_SCALE));
 		if(mem.getActive())
 		{
 			thisTextArea.setBackground(Color.GREEN);
@@ -79,27 +113,58 @@ public class BinaryBuddySystemGUI
 		{
 			thisTextArea.setBackground(Color.RED);
 		}
-		thisTextArea.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 13));
-		thisTextArea.append(mem.toString());
+		thisTextArea.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 12));
+		thisTextArea.append("  " + mem.toString());
+		
+		return thisTextArea;
+	}
+	
+	/**
+	 * Builds the starting JTextArea.
+	 * @return Returns the JTextArea.
+	 */
+	public JTextArea startPannel()
+	{
+		JTextArea thisTextArea = new JTextArea();
+		thisTextArea.setBackground(Color.BLACK);
+		thisTextArea.setForeground(Color.WHITE);
+		thisTextArea.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 18));
+		thisTextArea.append("Status: Ready");
 		
 		return thisTextArea;
 	}
 
+	/**
+	 * Gets the memory ArrayList
+	 * @return memArray
+	 */
 	public ArrayList getMemArray() 
 	{
 		return memArray;
 	}
 
+	/**
+	 * Sets the memory ArrayList
+	 * @param memArray The ArrayList to be set.
+	 */
 	public void setMemArray(ArrayList memArray) 
 	{
 		this.memArray = memArray;
 	}
 
+	/**
+	 * Gets the maxHeight
+	 * @return maxHeight
+	 */
 	public int getMaxHeight() 
 	{
 		return maxHeight;
 	}
 
+	/**
+	 * Sets the maxHeight
+	 * @param maxHeight
+	 */
 	public void setMaxHeight(int maxHeight) 
 	{
 		this.maxHeight = maxHeight;
